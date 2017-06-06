99c5b0fc74e82f75ea9e4228ffeb75cc754999bb
Achint08 <gr8achint.sharma@gmail.com>
2017-06-06 17:22:44 +0530

    Fixes #1 :- Builded an angular application

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MediaWallPage } from './app.po';
+
+describe('loklak-media-walls App', function () {
+
+		let page: MediaWallPage;
+		beforeEach(function () {
+				page = new MediaWallPage();
+		});
+
+		it('should display message saying Media walls Search!', () => {
+			page.navigateTo();
+			expect(page.getHeaderText()).toEqual('Media walls Search!');
+  	});
+});
